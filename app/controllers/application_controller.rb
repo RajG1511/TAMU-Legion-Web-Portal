fc0c4bb075c1be7afcd8d185dd57ba826c8532db
kylepalermo <kylepalermo@tamu.edu>
2025-09-24 21:32:55 -0500

    Added user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,19 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  # May be repetition of user model, I will have to see which approach is better.
-  def current_role_level
-    current_user&.role || 0
-  end
-
+  
   def require_member!
-    return if current_role_level >= 1
+    return if current_user&.member?
     redirect_to new_user_session_path, alert: 'Member access only.'
   end
 
   def require_exec!
-    return if current_role_level >= 2
+    return if current_user&.exec?
     redirect_to new_user_session_path, alert: 'Execuitive access only.'
   end
 
   def require_president!
-    return if current_role_level >= 3
+    return if current_user&.president?
     redirect_to new_user_session_path, alert: 'President access only.'
   end
 end
