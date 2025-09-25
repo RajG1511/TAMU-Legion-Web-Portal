a261e910e7988e611c98f310efe63c8d4a1f1365
kylepalermo <kylepalermo@tamu.edu>
2025-09-24 22:21:08 -0500

    added sign in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # idk if this works but a placeholder is needed
-  root 'rails/health#show'
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks",
     sessions: "users/sessions"
   }
+
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_24_220918) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_25_004624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -143,6 +143,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_24_220918) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
   add_foreign_key "committee_memberships", "committees"
