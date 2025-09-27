120d07c0b84a73062328ca55686f9e1943472f88
kylepalermo <kylepalermo@tamu.edu>
2025-09-26 22:10:03 -0500

    Removed strange files

--- db/schema.rb
+++ db/schema.rb
@@ -72,13 +72,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_213904) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.text "description"
-    t.datetime "starts_at", null: false
-    t.datetime "ends_at", null: false
+    t.datetime "starts_at"
+    t.datetime "ends_at"
     t.string "location"
-    t.bigint "event_category_id"
-    t.integer "visibility", default: 0, null: false
+    t.bigint "event_category_id", null: false
+    t.integer "visibility"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "published", default: 0, null: false
@@ -91,8 +91,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_213904) do
     t.string "location_text"
     t.index ["event_category_id"], name: "index_events_on_event_category_id"
     t.index ["published"], name: "index_events_on_published"
-    t.index ["starts_at"], name: "index_events_on_starts_at"
-    t.index ["visibility"], name: "index_events_on_visibility"
   end
 
   create_table "resource_categories", force: :cascade do |t|
@@ -114,46 +112,43 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_213904) do
   end
 
   create_table "resources", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.text "content"
-    t.integer "visibility", default: 0, null: false
-    t.bigint "resource_category_id"
+    t.integer "visibility"
+    t.bigint "resource_category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["resource_category_id"], name: "index_resources_on_resource_category_id"
-    t.index ["visibility"], name: "index_resources_on_visibility"
   end
 
   create_table "services", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.decimal "hours", precision: 5, scale: 2, null: false
-    t.string "name", null: false
+    t.decimal "hours"
+    t.string "name"
     t.text "description"
-    t.date "date_performed", null: false
-    t.integer "status", default: 0, null: false
+    t.date "date_performed"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["date_performed"], name: "index_services_on_date_performed"
-    t.index ["status"], name: "index_services_on_status"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
     t.integer "graduation_year"
     t.string "major"
     t.string "t_shirt_size"
-    t.integer "status", default: 1, null: false
+    t.integer "status"
     t.string "position"
-    t.integer "role", default: 0, null: false
+    t.integer "role"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["role"], name: "index_users_on_role"
-    t.index ["status"], name: "index_users_on_status"
+    t.string "provider"
+    t.string "uid"
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
   add_foreign_key "committee_memberships", "committees"
