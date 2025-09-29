6cd6ac014bb6a5ea7dacfaf6dee5c52f2bdaa4c9
kylepalermo <kylepalermo@tamu.edu>
2025-09-28 19:21:06 -0500

    added committee routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # idk if this works but a placeholder is needed
-  
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  
+
   # Event routes
   resources :events do
     member do
@@ -19,8 +19,8 @@ Rails.application.routes.draw do
       get :dashboard
     end
   end
-  
-    
+
+
   # Resource routes
   resources :resources do
     member do
@@ -31,12 +31,14 @@ Rails.application.routes.draw do
     end
   end
 
+  # Committee routes
+  resources :committees
+  resources :committee_memberships, only: [ :create, :destroy ]
 
-  # Defines the root path route ("/")
   root "home#index"
 
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   devise_scope :user do
