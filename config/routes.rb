ecee78ed887b9fc079d2b421d6a43d47b3b0498c
uzairak12 <uzairak12@tamu.edu>
2025-09-29 15:28:38 -0500

    adjusted routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get "/users/sign_in", to: "devise/sessions#new", as: :new_user_session
-    get "/users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+    get "/users/sign_in", to: "devise/sessions#new"
+    get "/users/sign_out", to: "devise/sessions#destroy"
   end
 
   # Services routes
