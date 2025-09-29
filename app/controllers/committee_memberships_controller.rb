8f326a1d8a87c0f253108ff26700d90f0a084c6d
kylepalermo <kylepalermo@tamu.edu>
2025-09-28 20:24:50 -0500

    added committee membership controller

---
+++ app/controllers/committee_memberships_controller.rb
@@ -0,0 +1,27 @@
+class CommitteeMembershipsController < ApplicationController
+  before_action :require_exec!
+  before_action :set_committee
+
+  def create
+    user = User.find(params[:user_id])
+    if @committee.committee_memberships.create!(user: user)
+      flash[:success] = "User #{user.name} added to committee #{@committee.name}."
+      redirect_to committee_path(@committee)
+    else
+      flash[:error] = "User #{user.name} not added to committee #{@committee.name}."
+      redirect_to committee_path(@committee)
+    end
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    @committee.committee_memberships.destroy_by(user: user)
+    flash[:success] = "User #{user.name} removed from committee #{@committee.name}."
+    redirect_to committee_path(@committee)
+  end
+
+  private
+  def set_committee
+    @committee = Committee.find(params[:committee_id])
+  end
+end
