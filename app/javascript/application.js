cd8fba44e90724c453d819cb875dd3389b34ba80
uzairak12 <uzairak12@tamu.edu>
2025-10-15 14:30:49 -0500

    took out import redundancy

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
 // Entry point for the build script in your package.json
 import Rails from "@rails/ujs"
 Rails.start()
-
-import "best_in_place"
-document.addEventListener("turbo:load", () => {
-  /* activate best_in_place */
-  $(".best_in_place").best_in_place();
-});
