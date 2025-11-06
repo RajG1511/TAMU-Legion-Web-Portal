f3e4328c915168d08b6a2e6f3bbcbe2a7763ec33
root <root@localhost>
2025-11-06 23:12:06 +0000

    minor dix

--- app/models/user_version.rb
+++ app/models/user_version.rb
@@ -1,6 +1,6 @@
 class UserVersion < ApplicationRecord
   belongs_to :user, optional: true          # actor
-  belongs_to :target_user, class_name: "User", optional: true
+  belongs_to :actor, class_name: "User", optional: true
 
   enum :change_type, { created: 0, updated: 1, deleted: 2, bulk_updated: 3, reset_inactive: 4 }
 
